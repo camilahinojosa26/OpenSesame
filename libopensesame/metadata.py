camilahinojosa26/OpenSesame

commit f9b808837004232641c1d25abb0165a516852ed9

Fix missing str.isascii in Python < 3.7

libopensesame/syntax.py
@@ -495,7 +495,7 @@ class syntax(object):
 			assert(s == s.strip())  # No flanking whitespace
 		except:
 			t = s.replace(u'_', u'')
-			if not t.isalnum() or not t.isascii():
+			if not t.isalnum() or isascii(t):
 				return u'"%s"' % s.replace(u'"', u'\\"')
 		return s
 

libopensesame/py3compat.py
@@ -34,6 +34,13 @@ else:
 	RecursionError = RuntimeError
 	PermissionError = IOError
 	FileNotFoundError = OSError
+	
+try:
+	str.isascii
+	isascii = lambda s: s.isascii()
+except AttributeError:  # str.isascii doesn't exist before Python 3.7
+	import string
+	isascii = lambda s: all(ch in string.printable for ch in s)
 
 
 def safe_decode(s, enc='utf-8', errors='strict'):
@@ -111,7 +118,8 @@ __all__ = [
 	'universal_newline_mode',
 	'safe_read',
 	'safe_open',
-	'safe_yaml_load'
+	'safe_yaml_load',
+	'isascii'
 ]
 if not py3:
 	__all__ += [

commit b4520e77d2e1120178cd706c505a8ed00d4fe73c

Fix regression in syntax introduced in f9b8088 - Bump to 3.3.10a14

libopensesame/metadata.py
@@ -21,7 +21,7 @@ from libopensesame.py3compat import *
 from distutils.version import StrictVersion
 import sys
 
-__version__ = u'3.3.10a13'
+__version__ = u'3.3.10a14'
 strict_version = StrictVersion(__version__)
 # The version without the prerelease (if any): e.g. 3.0.0
 main_version = u'.'.join([str(i) for i in strict_version.version])

libopensesame/syntax.py
@@ -495,7 +495,7 @@ class syntax(object):
 			assert(s == s.strip())  # No flanking whitespace
 		except:
 			t = s.replace(u'_', u'')
-			if not t.isalnum() or isascii(t):
+			if not t.isalnum() or not isascii(t):
 				return u'"%s"' % s.replace(u'"', u'\\"')
 		return s
 